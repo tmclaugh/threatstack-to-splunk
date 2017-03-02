7c308e4aeecc34b2ca985f4ef2ee1b9f8290238d
Tom McLaughlin <tom.mclaughlin@threatstack.com>
2017-03-02 15:48:22 -0500

    Add missing config value

--- config.py
+++ config.py
@@ -3,5 +3,6 @@ THREATSTACK_API_KEY = os.environ.get('THREATSTACK_API_KEY')
 THREATSTACK_BASE_URL = os.environ.get('THREATSTACK_BASE_URL', 'https://app.threatstack.com/api/v1')
 
 SPLUNK_API_TOKEN = os.environ.get('SPLUNK_API_TOKEN')
+SPLUNK_APP_NAME = os.environ.get('SPLUNK_APP_NAME', 'threatstack-to-splunk')
 SPLUNK_BASE_URL = os.environ.get('SPLUNK_BASE_URL')
 
